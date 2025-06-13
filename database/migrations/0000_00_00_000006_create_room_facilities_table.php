5620117fb1c316d7bbbbe7fd62857efde65140aa
Adika Setyadharma Susilo <susilo.2308561088@student.unud.ac.id>
2025-06-14 00:59:16 +0800

    chore: - fixed minor mistype

--- database/migrations/0000_00_00_000006_create_room_facilities_table.php
+++ database/migrations/0000_00_00_000006_create_room_facilities_table.php
@@ -1,3 +1,4 @@
+<?php
 // File: database/migrations/0000_00_00_000006_create_room_facilities_table.php
 
 use Illuminate\Database\Migrations\Migration;
@@ -8,7 +9,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('room_features', function (Blueprint $table) {
+        Schema::create('room_facilities', function (Blueprint $table) {
             $table->id();
             $table->foreignId('room_id')->constrained()->onDelete('cascade');
             $table->string('name');
