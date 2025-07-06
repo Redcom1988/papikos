140ca2a4e9ef39b9d34bbef09e79e6dcbf235e3b
Adika Setyadharma Susilo <susilo.2308561088@student.unud.ac.id>
2025-07-06 19:40:02 +0800

    chore: - separated dashboard message controller bits with api - renamed report controller to fit with other

--- app/Http/Controllers/Dashboard/ReportController.php
+++ app/Http/Controllers/Dashboard/ReportController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
-class ReportsController extends Controller
+class ReportController extends Controller
 {
     public function index(Request $request)
     {
